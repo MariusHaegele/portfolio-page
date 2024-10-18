4bd3a256db8a333f4114a9f33dbf184838264c09
MariusHaegele <marius.haegele@students.bfh.ch>
2024-10-18 14:34:38 +0200

    Anpassung px zu rem

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,28 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the button
+    let mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    }
+
+    // Add event listener to the button
+    mybutton.addEventListener('click', topFunction);
+});
